e71128289ee6c016e0c800032bea6e63811913b4
Isabelle Motte <38882780+zabellemotte@users.noreply.github.com>
2024-01-08 17:18:58 +0100

    customprefs block correct structure

---
+++ block_customprefs.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_news_items', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_customprefs
+ * @copyright 2024 zabelle_motte
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['customprefs:addinstance'] = 'Add a "Custom preferences" block';
+$string['pluginname'] = 'Custom preferences';
+$string['privacy:metadata'] = 'The Custom Preferences block only shows data stored in the customprefs assignement feedback and does not store data itself.';
+$string['blocktitle'] = 'Custom preferences';
+$string['option1'] = 'Option1';
+$string['option2'] = 'Option2';
+$string['option3'] = 'Option3';
+$string['osotext'] = 'Option selection'; // oso=option selection option
+$string['csotext'] = 'Courses selection'; // cso=course selection option
+$string['save'] = 'Save'; 
+$string['ocitext'] = 'Selected option: '; // oci = option choice introdution 
+$string['ccitext'] = 'Selected course(s): '; //cci = course choice introduction 
+
